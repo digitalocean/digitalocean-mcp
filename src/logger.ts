fa0feb8f3e40d4f361bd4577fae2b5fe44bd19c9
Davi <davibarros1994@gmail.com>
2025-04-25 16:31:57 -0400

    updates logger

--- src/logger.ts
+++ src/logger.ts
@@ -4,10 +4,6 @@ interface LogEntry {
   timestamp: string;
 }
 
-function logMessage(entry: LogEntry) {
-  process.stderr.write(`${JSON.stringify(entry)}\n`);
-}
-
 function toMessage(...args: Parameters<typeof console.error>): string {
   return args
     .map((arg) => (typeof arg === "object" ? JSON.stringify(arg) : String(arg)))
@@ -16,25 +12,36 @@ function toMessage(...args: Parameters<typeof console.error>): string {
 
 const logger = {
   info: (...args: any[]) => {
-    logMessage({
+    const entry: LogEntry = {
       level: "info",
       message: toMessage(...args),
       timestamp: new Date().toISOString(),
-    });
+    };
+    process.stdout.write(`${JSON.stringify(entry)}\n`);
   },
   error: (...args: any[]) => {
-    logMessage({
+    const entry: LogEntry = {
       level: "error",
       message: toMessage(...args),
       timestamp: new Date().toISOString(),
-    });
+    };
+    process.stderr.write(`${JSON.stringify(entry)}\n`);
+  },
+  warn: (...args: any[]) => {
+    const entry: LogEntry = {
+      level: "warn",
+      message: toMessage(...args),
+      timestamp: new Date().toISOString(),
+    };
+    process.stderr.write(`${JSON.stringify(entry)}\n`);
   },
   debug: (...args: any[]) => {
-    logMessage({
+    const entry: LogEntry = {
       level: "debug",
       message: toMessage(...args),
       timestamp: new Date().toISOString(),
-    });
+    };
+    process.stdout.write(`${JSON.stringify(entry)}\n`);
   },
 };
 
