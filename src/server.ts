295955cd128c3d0b42fb9531bf818cef76488276
Davi <davibarros1994@gmail.com>
2025-04-25 17:25:45 -0400

    adds version number to server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { registerAppTools } from "./tools/app";
-
 import { DOMcpServer } from "./DOMcpServer";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import logger from "./logger";
+import pkg from "../package.json";
 
 export function createServer(): McpServer {
   const server = new DOMcpServer({
     name: "DigitalOcean MCP Server",
-    version: "0.1.0",
+    version: pkg.version,
   });
 
   registerAppTools(server);
@@ -18,6 +18,6 @@ export function createServer(): McpServer {
 export function startStdioServer(server: McpServer) {
   const transport = new StdioServerTransport();
   return server.connect(transport).then(() => {
-    logger.info("DigitalOcean MCP Server started ...");
+    logger.info(`DigitalOcean MCP Server v${pkg.version} started`);
   });
 }
